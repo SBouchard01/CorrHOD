5e7465d10652580c360800d0ce8226e1644c55a4
SBouchard01 <simonbouchard47@gmail.com>
2023-07-10 09:37:49 -0700

    Modified __init__

---
+++ CorrHOD/__init__.py
@@ -0,0 +1,6 @@
+from CorrHOD.cubic import CorrHOD_cubic
+from CorrHOD.cutsky import CorrHOD_cutsky
+
+from CorrHOD.catalogs import read_fits, catalog_cuts, create_random
+
+from CorrHOD.logging import create_logger
