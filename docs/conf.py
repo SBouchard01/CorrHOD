f77de3695a228354ce700b2c96416a75736ae956
SBouchard01 <simonbouchard47@gmail.com>
2023-07-10 14:18:57 -0700

    added mock option fix

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_book_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
