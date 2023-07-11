04381f4bff916d544ebcfb263791e362d929861a
SBouchard01 <simonbouchard47@gmail.com>
2023-07-11 08:01:02 -0700

    mocked corrfunc (bug in abacus install)

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Autodoc configuration ---------------------------------------------------
 # Mock imports that can't be resolved during documentation build
-autodoc_mock_imports = ['cosmoprimo', 'mockfactory', 'pycorr']
+autodoc_mock_imports = ['cosmoprimo', 'mockfactory', 'pycorr', 'Corrfunc']
 
 
 # -- Options for HTML output -------------------------------------------------
