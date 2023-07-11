d82698b75d21f7debc3adb66b6ad1d9435f76761
SBouchard01 <simonbouchard47@gmail.com>
2023-07-11 08:58:10 -0700

    changed theme

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ autodoc_mock_imports = ['cosmoprimo', 'mockfactory', 'pycorr']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 # html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
