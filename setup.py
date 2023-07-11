e2b7fef20f2cc87dfb5377e96e106327ef4de17c
SBouchard01 <simonbouchard47@gmail.com>
2023-07-11 07:49:04 -0700

    added docs requirements

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ import os
 install_requires=[
         'numpy',
         'pandas',
-        'abacusutils',
         'densitysplit @ git+https://github.com/epaillas/densitysplit@openmp',        
     ]
 
-if not os.getenv('READTHEDOCS'):
+if os.getenv('READTHEDOCS'):
+    install_requires.append('abacusutils[docs]')
+    
+else:
+    install_requires.append('abacusutils')
     install_requires.append('cosmoprimo[class,camb,astropy,extras] @ git+https://github.com/cosmodesi/cosmoprimo')
     install_requires.append('mockfactory @ git+https://github.com/cosmodesi/mockfactory')
     install_requires.append('pycorr[mpi,jackknife,corrfunc] @ git+https://github.com/cosmodesi/pycorr')
@@ -30,5 +33,10 @@ setup(
     python_requires='>=3.6, <4',
     license = 'MIT',
     include_package_data = True,
-    install_requires = install_requires
+    install_requires = install_requires,
+    extras_require = {
+        "docs": [
+            
+        ]
+    },
 )
